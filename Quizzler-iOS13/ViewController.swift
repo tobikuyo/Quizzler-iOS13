63c10a9083c48b7ec88d51a3e87cfad56fb47b67
Tobi Tselios <tobi.tselios@gmail.com>
2019-11-24 23:48:11 +0000

    Update questions in quiz

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    let quiz = [
+        [" 4 + 2 is equal to 6", "True"],
+        ["5 - 3 is greater than 1", "True"],
+        ["3 + 8 is less than 10", "False"]
+    ]
+    
+    var questionNumber = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        updateQuestion()
     }
-
-
-}
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle
+        let actualAnswer = quiz[questionNumber][1]
+        
+        if userAnswer == actualAnswer {
+            print("Right!")
+        } else {
+            print("Wrong!")
+        }
+        
+        if questionNumber + 1 < quiz.count {
+            questionNumber += 1
+        } else {
+            questionNumber = 0
+        }
+            updateQuestion()
+    }
+    
+    func updateQuestion() {
+        questionLabel.text = quiz[questionNumber][0]
+    }
+}  
 
